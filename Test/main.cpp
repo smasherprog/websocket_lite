2fcf2c5a2a9b3b5a5f025b422d2c2ff8140f907c
Scott <smasherprog@gmail.com>
2017-05-22 20:25:57 -0700

    updated test and removed bench test

--- Test/main.cpp
+++ Test/main.cpp
@@ -19,7 +19,7 @@ void wssautobahntest() {
         SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onConnection");
     });
     listener.onMessage([&](const SL::WS_LITE::WSocket& socket, const SL::WS_LITE::WSMessage& message) {
-     //   SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onMessage");
+        //   SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onMessage");
         SL::WS_LITE::WSMessage msg;
         msg.Buffer = std::shared_ptr<unsigned char>(new unsigned char[message.len], [](unsigned char* p) { delete[] p; });
         msg.len = message.len;
@@ -36,49 +36,49 @@ void wssautobahntest() {
 
     SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "Exiting autobahn test...");
 }
+void generaltest() {
+    //auto listener = SL::WS_LITE::WSListener::CreateListener(3001, TEST_CERTIFICATE_PRIVATE_PASSWORD, TEST_CERTIFICATE_PRIVATE_PATH, TEST_CERTIFICATE_PUBLIC_PATH, TEST_DH_PATH);
+    auto listener = SL::WS_LITE::WSListener::CreateListener(3001);
+    auto lastheard = std::chrono::high_resolution_clock::now();
+    listener.onHttpUpgrade([&](const SL::WS_LITE::WSocket& socket) {
+        lastheard = std::chrono::high_resolution_clock::now();
+        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onHttpUpgrade");
 
+    });
+    listener.onConnection([&](const SL::WS_LITE::WSocket& socket, const std::unordered_map<std::string, std::string>& header) {
+        lastheard = std::chrono::high_resolution_clock::now();
+        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onConnection");
+
+    });
+    listener.onDisconnection([&](const SL::WS_LITE::WSocket& socket, unsigned short code, const std::string& msg) {
+        lastheard = std::chrono::high_resolution_clock::now();
+        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onDisconnection");
+    });
+    listener.startlistening();
+
+    //auto client = SL::WS_LITE::WSClient::CreateClient(TEST_CERTIFICATE_PUBLIC_PATH);
+    auto client = SL::WS_LITE::WSClient::CreateClient();
+    client.onHttpUpgrade([&](const SL::WS_LITE::WSocket& socket) {
+        lastheard = std::chrono::high_resolution_clock::now();
+        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "Client::onHttpUpgrade");
+
+    });
+    client.onConnection([&](const SL::WS_LITE::WSocket& socket, const std::unordered_map<std::string, std::string>& header) {
+        lastheard = std::chrono::high_resolution_clock::now();
+        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "Client::onConnection");
+    });
+    client.onDisconnection([&](const SL::WS_LITE::WSocket& socket, unsigned short code, const std::string& msg) {
+        lastheard = std::chrono::high_resolution_clock::now();
+        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "client::onDisconnection");
+    });
+    client.connect("localhost", 3001);
+
+    while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - lastheard).count() < 2000) {
+        std::this_thread::sleep_for(200ms);
+    }
+}
 int main(int argc, char* argv[]) {
     wssautobahntest();
-
+    generaltest();
     return 0;
 }
-//
-//
-//int main(int argc, char* argv[]) {
-//
-//    //auto listener = SL::WS_LITE::WSListener::CreateListener(3001, TEST_CERTIFICATE_PRIVATE_PASSWORD, TEST_CERTIFICATE_PRIVATE_PATH, TEST_CERTIFICATE_PUBLIC_PATH, TEST_DH_PATH);
-//    auto listener = SL::WS_LITE::WSListener::CreateListener(3001);
-//    listener.onHttpUpgrade([](const SL::WS_LITE::WSocket& socket) {
-//        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onHttpUpgrade");
-//
-//    });
-//    listener.onConnection([](const SL::WS_LITE::WSocket& socket, const std::unordered_map<std::string, std::string>& header) {
-//        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onConnection");
-//
-//    });
-//    listener.onDisconnection([](const SL::WS_LITE::WSocket& socket, unsigned short code, const std::string& msg) {
-//        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "listener::onDisconnection");
-//    });
-//    listener.startlistening();
-//
-//    //auto client = SL::WS_LITE::WSClient::CreateClient(TEST_CERTIFICATE_PUBLIC_PATH);
-//    auto client = SL::WS_LITE::WSClient::CreateClient();
-//    bool waitingconnection = true;
-//    client.onHttpUpgrade([](const SL::WS_LITE::WSocket& socket) {
-//        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "Client::onHttpUpgrade");
-//
-//    });
-//    client.onConnection([&](const SL::WS_LITE::WSocket& socket, const std::unordered_map<std::string, std::string>& header) {
-//        waitingconnection = false;
-//        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "Client::onConnection");
-//    });    
-//    client.onDisconnection([](const SL::WS_LITE::WSocket& socket, unsigned short code, const std::string& msg) {
-//        SL_WS_LITE_LOG(SL::WS_LITE::Logging_Levels::INFO_log_level, "client::onDisconnection");
-//    });
-//    client.connect("localhost", 3001);
-//
-//    while (true) {
-//        std::this_thread::sleep_for(1s);
-//    }
-//    return 0;
-//}
